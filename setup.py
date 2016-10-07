0db1d25f399005e180c3bc2823ededf9dcbb917d
ZwEin <zwein27@gmail.com>
2016-10-07 13:38:02 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 # @Author: ZwEin
 # @Date:   2016-09-30 14:01:47
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-07 01:11:43
+# @Last Modified time: 2016-10-07 13:36:14
 
 
 from distutils.core import setup
 from setuptools import Extension,find_packages
 from os import path
+from digBusinessTypeExtractor.documentation_classification import init_env
+
+init_env()
 
 setup(
     name = 'digBusinessTypeExtractor',
@@ -18,6 +21,7 @@ setup(
     url = 'https://github.com/usc-isi-i2/dig-business-type-extractor',
     download_url = 'https://github.com/usc-isi-i2/dig-business-type-extractor',
     packages = find_packages(),
+    package_data={'digBusinessTypeExtractor': ['data/*.csv']},
     keywords = ['business', 'type', 'extractor'],
-    install_requires=['digExtractor', 'digCrfTokenizer', 'digDictionaryExtractor']
+    install_requires=['digExtractor', 'numpy', 'scipy', 'scikit-learn']
     )
