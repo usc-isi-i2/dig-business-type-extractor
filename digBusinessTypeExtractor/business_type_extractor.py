2bd85dc6329d7cf5593920699c9b713755c003c4
ZwEin <zwein27@gmail.com>
2016-10-07 13:32:31 -0700

    persistancy and refine train/test funcs

--- digBusinessTypeExtractor/business_type_extractor.py
+++ digBusinessTypeExtractor/business_type_extractor.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-10-03 23:46:09
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-07 00:38:19
+# @Last Modified time: 2016-10-07 12:17:42
 
 import copy 
 import types
@@ -14,10 +14,11 @@ class BusinessTypeExtractor(Extractor):
     def __init__(self):
         self.renamed_input_fields = ['text']
         
-    def extract(self, doc):
+    def extract(self, doc, wedc_obj=None):
         if 'text' in doc:
-            dc = WEDC(vectorizer_type='count', classifier_type='knn')
-            return dc.predict(doc['text'])
+            if not wedc_obj:
+                wedc_obj = WEDC(vectorizer_type='count', classifier_type='knn')
+            return wedc_obj.predict(doc['text'])
         return None
 
     def get_metadata(self):
--- digBusinessTypeExtractor/documentation_classification.py
+++ digBusinessTypeExtractor/documentation_classification.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-10-06 23:36:45
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-07 00:44:15
+# @Last Modified time: 2016-10-07 13:31:44
 # -*- coding: utf-8 -*-
 # @Author: ZwEin
 # @Date:   2016-09-23 12:58:37
@@ -13,10 +13,13 @@ import os
 import sys
 import csv
 import numpy as np
+from sklearn.externals import joblib
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from os.path import expanduser
+import shutil
 
 
 ##################################################################
@@ -38,7 +41,26 @@ DC_CATEGORY_DICT = {
     'job_ads': 4
 }
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+USER_HOME_PATH = expanduser("~")
+
+APP_DIR = os.path.join(USER_HOME_PATH, 'WEDC_DATA')
+DATA_DIR = os.path.join(APP_DIR, 'data')
+MODEL_DIR = os.path.join(APP_DIR, 'model')
+MODEL_CLASSIFIER_DIR = os.path.join(MODEL_DIR, 'classifier')
+MODEL_VECTORIZER_DIR = os.path.join(MODEL_DIR, 'vectorizer')
+
+DC_DEFAULT_DATASET_PATH = os.path.join(DATA_DIR, 'dataset.csv')
+DC_DEFAULT_CLASSIFIER_MODEL_PATH = os.path.join(MODEL_CLASSIFIER_DIR, 'wedc_classifier.pkl')
+DC_DEFAULT_VECTORIZER_MODEL_PATH = os.path.join(MODEL_VECTORIZER_DIR, 'wedc_vectorizer.pkl')
+
+def init_env():
+    for path in [DATA_DIR, MODEL_CLASSIFIER_DIR, MODEL_VECTORIZER_DIR]:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    default_data_path = os.path.join(os.path.dirname(__file__), 'data', 'dataset.csv')
+    if not os.path.exists(DC_DEFAULT_DATASET_PATH):
+        shutil.copy(default_data_path, DC_DEFAULT_DATASET_PATH)
 
 class WEDC(object):
 
@@ -46,7 +68,7 @@ class WEDC(object):
     # Basic Methods
     ##################################################################
 
-    def __init__(self, data_path=os.path.join(DATA_DIR, 'dataset.csv'), vectorizer_type='count', classifier_type='knn'):
+    def __init__(self, data_path=DC_DEFAULT_DATASET_PATH, classifier_model_path=None, vectorizer_model_path=None, vectorizer_type='count', classifier_type='knn'):
         self.corpus = []
         self.labels = []
         self.size = 0
@@ -59,6 +81,12 @@ class WEDC(object):
 
         self.vectorizer = self.load_vectorizer(handler_type=vectorizer_type, binary=True)
         self.classifier = self.load_classifier(handler_type=classifier_type, weights='distance', n_neighbors=5, metric='jaccard')
+
+        if not classifier_model_path:
+            self.classifier_model_path = DC_DEFAULT_CLASSIFIER_MODEL_PATH
+
+        if not vectorizer_model_path:
+            self.vectorizer_model_path = DC_DEFAULT_VECTORIZER_MODEL_PATH
 
     def load_data(self, filepath=None):
         dataset = []
@@ -89,46 +117,46 @@ class WEDC(object):
         }
         return classifiers[handler_type]
 
-    def predict(self, test_data):
-        if not test_data :
-            return None
 
-        train_data_corpus, train_data_labels = self.corpus, self.labels
-        test_data_corpus = [test_data] if isinstance(test_data, basestring) else test_data
+    def train(self, data_path=None):
+        if data_path:
+            new_corpus, new_labels = self.load_data(filepath=data_path)
+            self.corpus += new_corpus
+            self.labels += new_labels
+            self.size += len(new_labels)
 
-        train_data_size = len(train_data_corpus)
-        test_data_size = len(test_data_corpus)
+        vectors = self.vectorizer.fit_transform(self.corpus).toarray()
 
-        corpus = train_data_corpus + test_data_corpus
-        labels = train_data_labels
-        size = train_data_size + test_data_size
-
-        vectors = self.vectorizer.fit_transform(corpus).toarray()
-
-        train_X = vectors[:train_data_size]
-        train_y = labels
-        test_X = vectors[train_data_size:]
-
-        self.classifier.fit(train_X, train_y)
-        pred_y = self.classifier.predict(test_X)
-        pred_y = [DC_CATEGORY_NAMES[int(i)] for i in pred_y]
-        return pred_y
-
+        self.classifier.fit(vectors, self.labels)
+        joblib.dump(self.classifier, self.classifier_model_path) 
+        joblib.dump(self.vectorizer, self.vectorizer_model_path) 
+        # DC_DEFAULT_VECTORIZER_MODEL_PATH
         
+        return self.classifier, self.vectorizer
 
+    def predict(self, data, classifier_model_path=None, vectorizer_model_path=None):
+        if not data :
+            return None
+        if not classifier_model_path:
+            classifier_model_path = self.classifier_model_path
+        if not vectorizer_model_path:
+            vectorizer_model_path = self.vectorizer_model_path
+
+        data_corpus = [data] if isinstance(data, basestring) else data
+
+        # load trained classifier and vectorizer models
+        classifier = vectorizer = None
+        try:
+            classifier = joblib.load(classifier_model_path) 
+            vectorizer = joblib.load(vectorizer_model_path)
+        except:
+            classifier, vectorizer = self.train()
+            
+        data_corpus = vectorizer.transform(data_corpus).toarray()
+        
+        return [DC_CATEGORY_NAMES[int(i)] for i in classifier.predict(data_corpus)]
 
 if __name__ == '__main__':
-
-    ## Train and test path provided separately
-    # dc = WEDC(vectorizer_type='count', classifier_type='knn')
-    # train_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/training-data/dig_memex_eval_datasets.csv'
-    # test_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/testing-data/testing_data.csv'
-    # dc.run(train_data_path=train_path, test_data_path=test_path)
-
-    ## Only one data path provided
-    
-    # data_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/testing-data/testing_data.csv'
-    
 
     test_data = ["   DOWNTOWN NEW HOT ASIAN ANGEL 100% REAL PICS Sexy Face Young  - 19      - Click to save or unsave                                                    Posted:  4 months ago                                        Age:  19                                   Category:   Phoenix Escorts                    Hi, Guys,   My Name is Lydia, and I new to the area,   I am a Korean and Spanish mix college student.only paritime, I am 20yrs, 5'4, 34D-23-35.   You will enjoy our time together guaranteed. 100% me.. *I'm ""the REAL deal""* I'm a Sweet, FUN playmate that knows how to have a good time!   Never Say No!! Never Rush!!  Call or Txt: 929-272-7898, TXT: 647-687-7096, Wechat: aa5854660383                        (929) 272-7898                      |  929.272.7898                      |  929-272-7898                      |  (929)272-7898                      |  9292727898     Flag this ad     Hi, Guys, My Name is Lydia, and I new to the area, I am a Korean and Spanish mix college student.only paritime, I am 20yrs, 5'4, 34D-23-35. You will enjoy our time together guaranteed. 100% me.. *I'm ""the REAL deal""* I'm a Sweet, FUN playmate that knows how to have a good time! Never Say No!! Never Rush!! Call or Txt: 929-272-7898, TXT: 647-687-7096, Wechat: aa5854660383 DOWNTOWN NEW HOT ASIAN ANGEL 100% REAL PICS Sexy Face Young  - 19 DOWNTOWN NEW HOT ASIAN ANGEL 100% REAL PICS Sexy Face Young  - 19 - A Sexy Service.com"]
 
@@ -136,5 +164,6 @@ if __name__ == '__main__':
     # data_path = os.path.join(DATA_DIR, 'dataset.csv') # include all training and testing dataset
     dc = WEDC(vectorizer_type='count', classifier_type='knn')
     print dc.predict(test_data)
-
+    # print dc.train()
+    # init_env()
 
